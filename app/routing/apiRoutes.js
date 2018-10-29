494ece930193bd115cbf9235b200011c90595ede
Urvish Patel <urvishp13@gmail.com>
2018-10-29 10:17:03 -0400

    got modal working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,34 +14,43 @@ module.exports = function(app) {
     friends.push(newPerson)
     // saving the scores to an array variable
     const scores = req.body.scores;
+    // convert all the strings to numbers
+    for (var i = 0; i < scores.length; i++) {
+      scores[i] = parseInt(scores[i])
+    }
     // creating a variable to hold all the differences between people's scores
     var allFriendsDifferences = [];
-
-    // comparing the scores to the scores of friends
-    for (var i = 0; i < friends.length; i++) {
-      console.log(i);
+    console.log("Number of friends = " + friends.length);
+    // comparing the user's scores to the friends' scores
+    for (var i = 0; i < friends.length - 1; i++) {
+      console.log("Friend: " + friends[i].name);
       // scores of one friend
-      var scoreToCompare = friends[i].scores
-      // make an array to hold the differences between the scores in the same places in the arrays
+      var scoresToCompare = friends[i].scores
+      // make an array to hold the differences between the scores in the same places in the scores and scoresToCompare arrays
       var totalDifferenceArray = [];
 
       // go through an array of the length of scores
       for (var j = 0; j < scores.length; j++) {
+        console.log("Score: " + scores[j])
+        console.log("Score of Friend: " + scoresToCompare[j]);
         // take the difference between the current user and the friend's score
-        var difference = scores[j] - scoreToCompare[j];
+        var difference = scores[j] - scoresToCompare[j];
         // make a case for when the difference is negative
         if (difference < 0) {
           difference = -1 * difference;
         }
+        console.log("Difference: " + difference);
         // put the score difference into the totalDifferenceArray
         totalDifferenceArray.push(difference)
+        console.log(totalDifferenceArray);
       }
-
+      var totalDifference = 0;
       // add all the values in the totalDifference array
-      for (var i = 0; i < totalDifferenceArray.length; i++) {
-        var totalDifference = totalDifference + totalDifferenceArray[i];
+      for (var k = 0; k < totalDifferenceArray.length; k++) {
+        totalDifference = totalDifference + parseInt(totalDifferenceArray[k]);
+        console.log(`Total difference at ${k}: ${totalDifferenceArray[k]}`);
+        console.log(`Total difference: ${totalDifference}`);
       }
-
       // add the difference to an array
       allFriendsDifferences.push(totalDifference);
     }
@@ -57,14 +66,11 @@ module.exports = function(app) {
       if (lowestNumber > allFriendsDifferences[i]) {
         lowestNumber = allFriendsDifferences[i];
         lowestNumberIndex = i;
-        // console.log(lowestNumber);
       } // else if lowestNumber < allFriendsDifferences[i], do nothing.
     }
 
-    // SEND BACK friends[lowestNumberIndex]. HOW DO I DO THIS?
-
-    // send back the res.json of the newPerson
-    res.json(newPerson)
+    // send back the friend with the closest similarities
+    res.json(friends[lowestNumberIndex]);
   });
 
 }
