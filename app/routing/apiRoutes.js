e8efcc6ac3b96abe470de55a0dc4508645c6ee71
Urvish Patel <urvishp13@gmail.com>
2018-10-29 10:31:28 -0400

    hiding the node_modules folder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,10 +20,8 @@ module.exports = function(app) {
     }
     // creating a variable to hold all the differences between people's scores
     var allFriendsDifferences = [];
-    console.log("Number of friends = " + friends.length);
     // comparing the user's scores to the friends' scores
     for (var i = 0; i < friends.length - 1; i++) {
-      console.log("Friend: " + friends[i].name);
       // scores of one friend
       var scoresToCompare = friends[i].scores
       // make an array to hold the differences between the scores in the same places in the scores and scoresToCompare arrays
@@ -31,25 +29,19 @@ module.exports = function(app) {
 
       // go through an array of the length of scores
       for (var j = 0; j < scores.length; j++) {
-        console.log("Score: " + scores[j])
-        console.log("Score of Friend: " + scoresToCompare[j]);
         // take the difference between the current user and the friend's score
         var difference = scores[j] - scoresToCompare[j];
         // make a case for when the difference is negative
         if (difference < 0) {
           difference = -1 * difference;
         }
-        console.log("Difference: " + difference);
         // put the score difference into the totalDifferenceArray
         totalDifferenceArray.push(difference)
-        console.log(totalDifferenceArray);
       }
       var totalDifference = 0;
       // add all the values in the totalDifference array
       for (var k = 0; k < totalDifferenceArray.length; k++) {
         totalDifference = totalDifference + parseInt(totalDifferenceArray[k]);
-        console.log(`Total difference at ${k}: ${totalDifferenceArray[k]}`);
-        console.log(`Total difference: ${totalDifference}`);
       }
       // add the difference to an array
       allFriendsDifferences.push(totalDifference);
